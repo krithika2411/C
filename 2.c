0ef2cd95917827e4995b3c9d53d3ae8278724a65
krithika2411 <krithisarva7@gmail.com>
2021-11-21 23:05:23 +0530

    2

---
+++ 2.c
@@ -0,0 +1,10 @@
+//Data types
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    printf("int: %ld\n", sizeof(int));
+    printf("float: %ld\n", sizeof(float));
+    printf("double: %ld\n", sizeof(double));
+    printf("char: %ld\n", sizeof(char));
+    return 0;
+}
---
+++ 3.c
@@ -0,0 +1,16 @@
+//var and constants
+#include <stdio.h>
+int main(){
+int a, b;
+float age = 18.5;
+char  letter = 'k';
+const double Pie = 3.14;
+a=19;
+b= 19;
+int c = a + b;
+printf("%d\n", c);
+printf("%f\n", age);
+printf("%c\n", letter);
+printf("%f", Pie);
+    return 0;
+}
