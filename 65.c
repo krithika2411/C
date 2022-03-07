15db2d278344043b31ceed826d05152e8c07e7f7
krithika2411 <krithisarva7@gmail.com>
2022-03-07 22:01:01 +0530

    ..

---
+++ 65.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+   int x= 10;
+   int y= 0;
+
+   if (y!=0)
+   {
+       printf("x/y=%d", x/y);
+   }
+   else printf("divisor is 0. program exiting");
+   exit(0);
+    return 0;
+}
