9f6175e0c69be799c54baf68e983b8a4d429bc38
krithika2411 <krithisarva7@gmail.com>
2022-03-08 02:33:59 +0530

    ..

---
+++ 66.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+extern int errno;
+int main(){
+    FILE *fptr;
+    errno = 0;
+    fptr = fopen("c:\\test.txt", "r");
+    if (fptr == NULL)
+    {
+        fprintf(stderr, "Error opening file. Error code: %d\n", errno);
+       exit (EXIT_FAILURE);
+    }
+    fclose(fptr);
+    return 0;
+}
