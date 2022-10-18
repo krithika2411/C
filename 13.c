06d4c8de6dbde843f4527e8ae78546023ea8431f
Vishesh Rawal <92795514+visheshrwl@users.noreply.github.com>
2022-10-18 14:20:27 +0530

    Update 13.c

--- 13.c
+++ 13.c
@@ -4,8 +4,8 @@ int main(){
     int profit= 9999;
     int clients=20;
     if (profit > 1000)
-    if (clients >15);
-
+    if (clients >15); //there will be no ; for if loop and also it's useless 
+    
     bonus =100;
     printf("profit: %d, profit");
     return 0;
